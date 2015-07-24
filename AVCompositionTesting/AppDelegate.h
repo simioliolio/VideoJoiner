627dea0462d10e76f098f452d2f8ffd9e406f3d6
simioliolio <simioliolio@bitbasic.co.uk>
2015-07-24 14:54:37 +0100

    Initial Commit

---
+++ AVCompositionTesting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AVCompositionTesting
+//
+//  Created by Simon Haycock on 24/07/2015.
+//  Copyright (c) 2015 oxygn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AVCompositionTesting/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AVCompositionTesting
+//
+//  Created by Simon Haycock on 24/07/2015.
+//  Copyright (c) 2015 oxygn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
